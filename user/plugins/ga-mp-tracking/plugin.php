86bbacfd178ed9caf7dc377ade1d7c9626ea4eb8
freuds <frederic.willien@gmail.com>
2018-01-24 14:57:33 +0100

    change order parameter for plugin GA

--- user/plugins/ga-mp-tracking/plugin.php
+++ user/plugins/ga-mp-tracking/plugin.php
@@ -39,13 +39,14 @@ function file_get_contents_curl($url) {
 
 // Handle the parsing of the _ga cookie or setting it to a unique identifier
 function power_ga_mp_gaParseCookie() {
-  if (isset($_COOKIE['_ga'])) {
-    list($version,$domainDepth, $cid1, $cid2) = explode('[\.]', $_COOKIE["_ga"],4);
-    $contents = array('version' => $version, 'domainDepth' => $domainDepth, 'cid' => $cid1.'.'.$cid2);
-    $cid = $contents['cid'];
-  }
-  else $cid = power_ga_mp_UUID();
-  return $cid;
+    if (isset($_COOKIE['_ga'])) {
+        list($version,$domainDepth, $cid1, $cid2) = explode('.', $_COOKIE["_ga"],4);
+        $contents = array('version' => $version, 'domainDepth' => $domainDepth, 'cid' => $cid1.'.'.$cid2);
+        $cid = $contents['cid'];
+    }
+        else $cid = power_ga_mp_UUID();
+
+    return $cid;
 }
   
 function power_ga_mp_UUID() {
@@ -60,28 +61,26 @@ function power_ga_mp_UUID() {
 
   
 // Our custom function that will be triggered when the event occurs
-function power_ga_mp($keyword, $title = '(unknown)', $referer = '') { 
-	global GAID;
+function power_ga_mp($keyword, $title = '(unknown)', $referer = '') {
 
-	$version = 1;
-	$z = rand(100000000000,999999999999); // Cache Buster  to ensure browsers and proxies don't cache hits
-	$power_ga_mp_GAID = GAID;
+    $version = 1;
+    $z = rand(100000000000,999999999999); // Cache Buster to ensure browsers and proxies don't cache hits
    
     $data = array(
         'v' => $version,
-        'tid' => $power_ga_mp_GAID,
+        'tid' => GAID,
         'cid' => power_ga_mp_gaParseCookie(),
         'uip' => $_SERVER['REMOTE_ADDR'],
-        'z' => $z,
         't' => 'pageview',
         'dh' => $_SERVER['SERVER_NAME'],
         'dp' => $keyword,
         'dt' => $title,
         'dr' => $referer,
+        'z' => $z,
     );
 
     if($data) {
-        $getString = 'https://ssl.google-analytics.com/collect';
+        $getString = 'https://www.google-analytics.com/collect';
         $getString .= '?payload_data&';
         $getString .= http_build_query($data);
         $result = file_get_contents_curl($getString);
