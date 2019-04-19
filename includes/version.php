f21099c5ac97b340b3eec4dcfe0ef88d8021accd
Fred Willien <fwillien@quotatis.com>
2019-04-19 15:52:55 +0200

    Merge YOURLS 1.7.3

--- includes/version.php
+++ includes/version.php
@@ -3,7 +3,11 @@
  * YOURLS version
  *
  */
+<<<<<<< HEAD
 define( 'YOURLS_VERSION', '1.7.2' );
+=======
+define( 'YOURLS_VERSION', '1.7.4' );
+>>>>>>> 7b097126f9e838c3eab1693531886fc168b1e8d9
 
 /**
  * YOURLS DB version. Increments when changes are made to the DB schema, to trigger a DB update
